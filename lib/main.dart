c755d39cf7c77e6b7f5368b0ac1c515ea87e6caa
Haruto Konno <qingdoujinye@gmail.com>
2021-11-02 15:44:34 +0900

    welcome_screenを追加

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_chat_app/screen/welcome_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,9 +10,22 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'flutter_chat_app',
-      home: HomePage(),
+      theme: ThemeData.dark().copyWith(
+        textTheme: const TextTheme(
+          bodyText1: TextStyle(color: Colors.black54)
+        ),
+      ),
+      initialRoute: 'longin_screen',
+      routes: {
+        const WelcomeScreen().id: (context) => const WelcomeScreen(),
+        // 'login_screen': (context) => LoginScreen(),
+        // 'registration_screen': (context) => RegistrationScreen(),
+        // 'chat_screen': (context) => ChatScreen(),
+      },
+      home: const HomePage(),
     );
   }
 }
---
+++ lib/screen/welcome_screen.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+
+class WelcomeScreen extends StatefulWidget {
+  const WelcomeScreen({Key? key}) : super(key: key);
+  final String id = 'welcome_screen';
+
+  @override
+  State<WelcomeScreen> createState() => _WelcomeScreenState();
+}
+
+class _WelcomeScreenState extends State<WelcomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 24.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Row(
+              children: <Widget>[
+                SizedBox(
+                  child: Image.asset('images/logo.png'),
+                  height: 60.0,
+                ),
+                const Text(
+                  'Flutter Chat App!',
+                  style: TextStyle(
+                    fontSize: 45.0,
+                    fontWeight: FontWeight.w800,
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 48.0,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 16.0),
+              child: Material(
+                elevation: 5.0,
+                color: Colors.lightBlueAccent,
+                borderRadius: BorderRadius.circular(30.0),
+                child: MaterialButton(
+                  onPressed: () {
+                    // ログイン画面に遷移
+                  },
+                  minWidth: 200,
+                  height: 42.0,
+                  child: const Text('ログイン'),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
