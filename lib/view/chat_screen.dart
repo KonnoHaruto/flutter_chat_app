fb75a2673396ce1f09b8efb45da818daa4471f49
Haruto Konno <qingdoujinye@gmail.com>
2021-11-09 22:45:08 +0900

    initState()を追加

--- lib/view/chat_screen.dart
+++ lib/view/chat_screen.dart
@@ -11,13 +11,22 @@ class ChatScreen extends StatefulWidget {
 
 class _ChatScreenState extends State<ChatScreen> {
   final _auth = FirebaseAuth.instance;
+  // FirebaseUser ~> User
   late User loggedInUser;
+
+  @override
+  void initState() {
+    super.initState();
+    getCurrentUser();
+  }
 
   void getCurrentUser() {
     try {
       final User? user = _auth.currentUser;
       if (user != null) {
         loggedInUser = user;
+        // ignore: avoid_print
+        print(loggedInUser.email);
       }
     } catch (e) {
       //ignore: avoid_print
@@ -29,6 +38,7 @@ class _ChatScreenState extends State<ChatScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        elevation: 0,
         leading: null,
         actions: <Widget>[
           IconButton(
@@ -38,7 +48,18 @@ class _ChatScreenState extends State<ChatScreen> {
             icon: const Icon(Icons.close),
           ),
         ],
+        title: const Text('Chat_App'),
+        backgroundColor: Colors.greenAccent,
       ),
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Container(),
+          ],
+        ),
+        ),
     );
   }
 }
