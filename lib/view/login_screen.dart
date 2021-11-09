9267fb819bb02d52ce197447515dfa1da0371843
Haruto Konno <qingdoujinye@gmail.com>
2021-11-09 23:02:21 +0900

    整形

--- lib/view/login_screen.dart
+++ lib/view/login_screen.dart
@@ -19,7 +19,9 @@ class _LoginScreenState extends State<LoginScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 24.0),
+        padding: const EdgeInsets.symmetric(
+          horizontal: 24.0,
+          ),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.stretch,
@@ -45,17 +47,32 @@ class _LoginScreenState extends State<LoginScreen> {
                 hintText: 'メールアドレスを入力',
                 hintStyle: TextStyle(color: Colors.black),
                 contentPadding:
-                    EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+                    EdgeInsets.symmetric(
+                      vertical: 10.0, 
+                      horizontal: 20.0,
+                      ),
                 border: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(32.0)),
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(32.0),
+                    ),
                 ),
                 enabledBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.blueAccent, width: 1.0),
-                  borderRadius: BorderRadius.all(Radius.circular(32.0)),
+                  borderSide: BorderSide(
+                    color: Colors.blueAccent, 
+                    width: 1.0,
+                    ),
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(32.0),
+                    ),
                 ),
                 focusedBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.blueAccent, width: 2.0),
-                  borderRadius: BorderRadius.all(Radius.circular(32.0)),
+                  borderSide: BorderSide(
+                    color: Colors.blueAccent, 
+                    width: 2.0,
+                    ),
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(32.0),
+                    ),
                 ),
               ),
             ),
@@ -74,17 +91,32 @@ class _LoginScreenState extends State<LoginScreen> {
                 hintText: 'パスワードを入力',
                 hintStyle: TextStyle(color: Colors.black),
                 contentPadding:
-                    EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+                    EdgeInsets.symmetric(
+                      vertical: 10.0, 
+                      horizontal: 20.0,
+                      ),
                 border: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(32.0)),
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(32.0),
+                    ),
                 ),
                 enabledBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.blueAccent, width: 1.0),
-                  borderRadius: BorderRadius.all(Radius.circular(32.0)),
+                  borderSide: BorderSide(
+                    color: Colors.blueAccent, 
+                    width: 1.0,
+                    ),
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(32.0),
+                    ),
                 ),
                 focusedBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.blueAccent, width: 2.0),
-                  borderRadius: BorderRadius.all(Radius.circular(32.0)),
+                  borderSide: BorderSide(
+                    color: Colors.blueAccent, 
+                    width: 2.0,
+                    ),
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(32.0),
+                    ),
                 ),
               ),
             ),
@@ -95,7 +127,9 @@ class _LoginScreenState extends State<LoginScreen> {
               padding: const EdgeInsets.symmetric(vertical: 16.0),
               child: Material(
                 color: Colors.blueAccent,
-                borderRadius: const BorderRadius.all(Radius.circular(30.0)),
+                borderRadius: const BorderRadius.all(
+                  Radius.circular(30.0),
+                  ),
                 elevation: 5.0,
                 child: MaterialButton(
                   onPressed: () {
--- lib/view/registration_screen.dart
+++ lib/view/registration_screen.dart
@@ -46,17 +46,32 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
                 hintText: 'メールアドレスを入力',
                 hintStyle: TextStyle(color: Colors.black),
                 contentPadding:
-                    EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+                    EdgeInsets.symmetric(
+                      vertical: 10.0, 
+                      horizontal: 20.0,
+                      ),
                 border: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(32.0)),
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(32.0),
+                    ),
                 ),
                 enabledBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.blueAccent, width: 1.0),
-                  borderRadius: BorderRadius.all(Radius.circular(32.0)),
+                  borderSide: BorderSide(
+                    color: Colors.blueAccent, 
+                    width: 1.0,
+                    ),
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(32.0),
+                    ),
                 ),
                 focusedBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.blueAccent, width: 2.0),
-                  borderRadius: BorderRadius.all(Radius.circular(32.0)),
+                  borderSide: BorderSide(
+                    color: Colors.blueAccent, 
+                    width: 2.0,
+                    ),
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(32.0),
+                    ),
                 ),
               ),
             ),
@@ -74,18 +89,32 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
               decoration: const InputDecoration(
                 hintText: 'パスワードを入力',
                 hintStyle: TextStyle(color: Colors.black),
-                contentPadding:
-                    EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+                contentPadding: EdgeInsets.symmetric(
+                  vertical: 10.0, 
+                  horizontal: 20.0,
+                  ),
                 border: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(32.0)),
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(32.0),
+                    ),
                 ),
                 enabledBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.blueAccent, width: 1.0),
-                  borderRadius: BorderRadius.all(Radius.circular(32.0)),
+                  borderSide: BorderSide(
+                    color: Colors.blueAccent, 
+                    width: 1.0,
+                    ),
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(32.0),
+                    ),
                 ),
                 focusedBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.blueAccent, width: 2.0),
-                  borderRadius: BorderRadius.all(Radius.circular(32.0)),
+                  borderSide: BorderSide(
+                    color: Colors.blueAccent, 
+                    width: 2.0,
+                    ),
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(32.0),
+                    ),
                 ),
               ),
             ),
