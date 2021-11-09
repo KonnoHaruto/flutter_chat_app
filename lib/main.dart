c13fb42ecdfa097ba923519cb53973bd6d257307
Haruto Konno <qingdoujinye@gmail.com>
2021-11-09 16:13:46 +0900

    修正

--- lib/main.dart
+++ lib/main.dart
@@ -3,13 +3,10 @@ import 'package:flutter/material.dart';
 import 'view/registration_screen.dart';
 import 'view/welcome_screen.dart';
 
-
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({ Key? key }) : super(key: key);
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
