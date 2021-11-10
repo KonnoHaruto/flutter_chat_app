5a45ab779088811d62b6c005c7a39a9d282d67b5
Haruto Konno <qingdoujinye@gmail.com>
2021-11-10 10:15:08 +0900

    ModalProgressHUDを追加

--- lib/view/login_screen.dart
+++ lib/view/login_screen.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_chat_app/view/chat_screen.dart';
+import 'package:modal_progress_hud_alt/modal_progress_hud_alt.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({Key? key}) : super(key: key);
@@ -12,150 +13,161 @@ class LoginScreen extends StatefulWidget {
 
 class _LoginScreenState extends State<LoginScreen> {
   final _auth = FirebaseAuth.instance;
+  bool showSpiner = false;
   late String email;
   late String password;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.symmetric(
-          horizontal: 24.0,
-        ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            const SizedBox(
-              height: 200.0,
-              child: Icon(
-                Icons.message,
-                color: Colors.black,
-              ),
-            ),
-            const SizedBox(
-              height: 48.0,
-            ),
-            TextField(
-              keyboardType: TextInputType.emailAddress,
-              textAlign: TextAlign.center,
-              onChanged: (value) {
-                //Do something with the user input.
-                email = value;
-              },
-              decoration: const InputDecoration(
-                hintText: 'メールアドレスを入力',
-                hintStyle: TextStyle(color: Colors.black),
-                contentPadding: EdgeInsets.symmetric(
-                  vertical: 10.0,
-                  horizontal: 20.0,
-                ),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(
-                    Radius.circular(32.0),
-                  ),
-                ),
-                enabledBorder: OutlineInputBorder(
-                  borderSide: BorderSide(
-                    color: Colors.blueAccent,
-                    width: 1.0,
-                  ),
-                  borderRadius: BorderRadius.all(
-                    Radius.circular(32.0),
-                  ),
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderSide: BorderSide(
-                    color: Colors.blueAccent,
-                    width: 2.0,
-                  ),
-                  borderRadius: BorderRadius.all(
-                    Radius.circular(32.0),
-                  ),
+      body: ModalProgressHUD(
+        inAsyncCall: showSpiner,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 24.0,
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              const SizedBox(
+                height: 200.0,
+                child: Icon(
+                  Icons.message,
+                  color: Colors.black,
                 ),
               ),
-            ),
-            const SizedBox(
-              height: 8.0,
-            ),
-            TextField(
-              textAlign: TextAlign.center,
-              obscureText: true,
-              style: const TextStyle(color: Colors.black),
-              onChanged: (value) {
-                //Do something with the user input.
-                password = value;
-              },
-              decoration: const InputDecoration(
-                hintText: 'パスワードを入力',
-                hintStyle: TextStyle(color: Colors.black),
-                contentPadding: EdgeInsets.symmetric(
-                  vertical: 10.0,
-                  horizontal: 20.0,
-                ),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(
-                    Radius.circular(32.0),
+              const SizedBox(
+                height: 48.0,
+              ),
+              TextField(
+                keyboardType: TextInputType.emailAddress,
+                textAlign: TextAlign.center,
+                onChanged: (value) {
+                  //Do something with the user input.
+                  email = value;
+                },
+                decoration: const InputDecoration(
+                  hintText: 'メールアドレスを入力',
+                  hintStyle: TextStyle(color: Colors.black),
+                  contentPadding: EdgeInsets.symmetric(
+                    vertical: 10.0,
+                    horizontal: 20.0,
                   ),
-                ),
-                enabledBorder: OutlineInputBorder(
-                  borderSide: BorderSide(
-                    color: Colors.blueAccent,
-                    width: 1.0,
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(32.0),
+                    ),
                   ),
-                  borderRadius: BorderRadius.all(
-                    Radius.circular(32.0),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.blueAccent,
+                      width: 1.0,
+                    ),
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(32.0),
+                    ),
                   ),
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderSide: BorderSide(
-                    color: Colors.blueAccent,
-                    width: 2.0,
-                  ),
-                  borderRadius: BorderRadius.all(
-                    Radius.circular(32.0),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.blueAccent,
+                      width: 2.0,
+                    ),
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(32.0),
+                    ),
                   ),
                 ),
               ),
-            ),
-            const SizedBox(
-              height: 24.0,
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 16.0),
-              child: Material(
-                color: Colors.blueAccent,
-                borderRadius: const BorderRadius.all(
-                  Radius.circular(30.0),
+              const SizedBox(
+                height: 8.0,
+              ),
+              TextField(
+                textAlign: TextAlign.center,
+                obscureText: true,
+                style: const TextStyle(color: Colors.black),
+                onChanged: (value) {
+                  //Do something with the user input.
+                  password = value;
+                },
+                decoration: const InputDecoration(
+                  hintText: 'パスワードを入力',
+                  hintStyle: TextStyle(color: Colors.black),
+                  contentPadding: EdgeInsets.symmetric(
+                    vertical: 10.0,
+                    horizontal: 20.0,
+                  ),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(32.0),
+                    ),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.blueAccent,
+                      width: 1.0,
+                    ),
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(32.0),
+                    ),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.blueAccent,
+                      width: 2.0,
+                    ),
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(32.0),
+                    ),
+                  ),
                 ),
-                elevation: 5.0,
-                child: MaterialButton(
-                  onPressed: () async {
-                    try {
-                      final user = await _auth.signInWithEmailAndPassword(
-                        email: email,
-                        password: password,
-                      );
-                      if (user != null) {
-                        Navigator.pushNamed(
-                          context, ChatScreen.id,
+              ),
+              const SizedBox(
+                height: 24.0,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 16.0),
+                child: Material(
+                  color: Colors.blueAccent,
+                  borderRadius: const BorderRadius.all(
+                    Radius.circular(30.0),
+                  ),
+                  elevation: 5.0,
+                  child: MaterialButton(
+                    onPressed: () async {
+                      setState(() {
+                        showSpiner = true;
+                      });
+                      try {
+                        final user = await _auth.signInWithEmailAndPassword(
+                          email: email,
+                          password: password,
                         );
+                        if (user != null) {
+                          Navigator.pushNamed(
+                            context,
+                            ChatScreen.id,
+                          );
+                        }
+                        setState(() {
+                          showSpiner = false;
+                        });
+                      } catch (e) {
+                        // ignore: avoid_print
+                        print(e);
                       }
-                    } catch (e) {
-                      // ignore: avoid_print
-                      print(e);
-                    }
-                  },
-                  minWidth: 200.0,
-                  height: 42.0,
-                  child: const Text(
-                    'ログイン',
-                    style: TextStyle(color: Colors.white),
+                    },
+                    minWidth: 200.0,
+                    height: 42.0,
+                    child: const Text(
+                      'ログイン',
+                      style: TextStyle(color: Colors.white),
+                    ),
                   ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
--- lib/view/registration_screen.dart
+++ lib/view/registration_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_chat_app/view/chat_screen.dart';
+import 'package:modal_progress_hud_alt/modal_progress_hud_alt.dart';
 
 class RegistrationScreen extends StatefulWidget {
   static String id = 'registration_screen';
@@ -13,147 +14,156 @@ class RegistrationScreen extends StatefulWidget {
 
 class _RegistrationScreenState extends State<RegistrationScreen> {
   final _auth = FirebaseAuth.instance;
+  bool showSpiner = false;
   late String email;
   late String password;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 24.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            const SizedBox(
-              height: 200.0,
-              child: Icon(
-                Icons.message,
-                color: Colors.black,
-              ),
-            ),
-            const SizedBox(
-              height: 48.0,
-            ),
-            TextField(
-              keyboardType: TextInputType.emailAddress,
-              textAlign: TextAlign.center,
-              onChanged: (value) {
-                //Do something with the user input.
-                email = value;
-              },
-              decoration: const InputDecoration(
-                hintText: 'メールアドレスを入力',
-                hintStyle: TextStyle(color: Colors.black),
-                contentPadding:
-                    EdgeInsets.symmetric(
-                      vertical: 10.0, 
-                      horizontal: 20.0,
-                      ),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(
-                    Radius.circular(32.0),
-                    ),
-                ),
-                enabledBorder: OutlineInputBorder(
-                  borderSide: BorderSide(
-                    color: Colors.blueAccent, 
-                    width: 1.0,
-                    ),
-                  borderRadius: BorderRadius.all(
-                    Radius.circular(32.0),
-                    ),
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderSide: BorderSide(
-                    color: Colors.blueAccent, 
-                    width: 2.0,
-                    ),
-                  borderRadius: BorderRadius.all(
-                    Radius.circular(32.0),
-                    ),
+      body: ModalProgressHUD(
+        inAsyncCall: showSpiner,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 24.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              const SizedBox(
+                height: 200.0,
+                child: Icon(
+                  Icons.message,
+                  color: Colors.black,
                 ),
               ),
-            ),
-            const SizedBox(
-              height: 8.0,
-            ),
-            TextField(
-              textAlign: TextAlign.center,
-              obscureText: true,
-              style: const TextStyle(color: Colors.black),
-              onChanged: (value) {
-                //Do something with the user input.
-                password = value;
-              },
-              decoration: const InputDecoration(
-                hintText: 'パスワードを入力',
-                hintStyle: TextStyle(color: Colors.black),
-                contentPadding: EdgeInsets.symmetric(
-                  vertical: 10.0, 
-                  horizontal: 20.0,
+              const SizedBox(
+                height: 48.0,
+              ),
+              TextField(
+                keyboardType: TextInputType.emailAddress,
+                textAlign: TextAlign.center,
+                onChanged: (value) {
+                  //Do something with the user input.
+                  email = value;
+                },
+                decoration: const InputDecoration(
+                  hintText: 'メールアドレスを入力',
+                  hintStyle: TextStyle(color: Colors.black),
+                  contentPadding: EdgeInsets.symmetric(
+                    vertical: 10.0,
+                    horizontal: 20.0,
                   ),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(
-                    Radius.circular(32.0),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(32.0),
                     ),
-                ),
-                enabledBorder: OutlineInputBorder(
-                  borderSide: BorderSide(
-                    color: Colors.blueAccent, 
-                    width: 1.0,
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.blueAccent,
+                      width: 1.0,
                     ),
-                  borderRadius: BorderRadius.all(
-                    Radius.circular(32.0),
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(32.0),
                     ),
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderSide: BorderSide(
-                    color: Colors.blueAccent, 
-                    width: 2.0,
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.blueAccent,
+                      width: 2.0,
                     ),
-                  borderRadius: BorderRadius.all(
-                    Radius.circular(32.0),
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(32.0),
                     ),
+                  ),
                 ),
               ),
-            ),
-            const SizedBox(
-              height: 24.0,
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 16.0),
-              child: Material(
-                color: Colors.blueAccent,
-                borderRadius: const BorderRadius.all(Radius.circular(30.0)),
-                elevation: 5.0,
-                child: MaterialButton(
-                  onPressed: () async {
-                    try {
-                      final newUser =
-                          await _auth.createUserWithEmailAndPassword(
-                        email: email,
-                        password: password,
-                      );
-                      // ignore: unnecessary_null_comparison
-                      if (newUser != null) {
-                        Navigator.pushNamed(context, ChatScreen.id);
+              const SizedBox(
+                height: 8.0,
+              ),
+              TextField(
+                textAlign: TextAlign.center,
+                obscureText: true,
+                style: const TextStyle(color: Colors.black),
+                onChanged: (value) {
+                  //Do something with the user input.
+                  password = value;
+                },
+                decoration: const InputDecoration(
+                  hintText: 'パスワードを入力',
+                  hintStyle: TextStyle(color: Colors.black),
+                  contentPadding: EdgeInsets.symmetric(
+                    vertical: 10.0,
+                    horizontal: 20.0,
+                  ),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(32.0),
+                    ),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.blueAccent,
+                      width: 1.0,
+                    ),
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(32.0),
+                    ),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.blueAccent,
+                      width: 2.0,
+                    ),
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(32.0),
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 24.0,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 16.0),
+                child: Material(
+                  color: Colors.blueAccent,
+                  borderRadius: const BorderRadius.all(Radius.circular(30.0)),
+                  elevation: 5.0,
+                  child: MaterialButton(
+                    onPressed: () async {
+                      setState(() {
+                        showSpiner = true;
+                      });
+                      try {
+                        final newUser =
+                            await _auth.createUserWithEmailAndPassword(
+                          email: email,
+                          password: password,
+                        );
+                        // ignore: unnecessary_null_comparison
+                        if (newUser != null) {
+                          Navigator.pushNamed(context, ChatScreen.id);
+                        }
+                        setState(() {
+                          showSpiner = false;
+                        });
+                      } catch (e) {
+                        //ignore: avoid_print
+                        print(e);
                       }
-                    } catch (e) {
-                      //ignore: avoid_print
-                      print(e);
-                    }
-                  },
-                  minWidth: 200.0,
-                  height: 42.0,
-                  child: const Text(
-                    '登録',
-                    style: TextStyle(color: Colors.white),
+                    },
+                    minWidth: 200.0,
+                    height: 42.0,
+                    child: const Text(
+                      '登録',
+                      style: TextStyle(color: Colors.white),
+                    ),
                   ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
