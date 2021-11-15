51c5a3f2a012dab024e878296483836b716d8417
Haruto Konno <qingdoujinye@gmail.com>
2021-11-15 18:59:56 +0900

    修正

--- lib/reference.dart
+++ lib/reference.dart
@@ -2,6 +2,3 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 
 CollectionReference chatRef 
     = FirebaseFirestore.instance.collection('messages');
-
-DocumentReference chatDocRef 
-    = chatRef.doc();
