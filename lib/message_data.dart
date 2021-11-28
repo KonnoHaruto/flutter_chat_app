04098665247fb53f8bc8d715493a90f58fb53d3b
Haruto Konno <qingdoujinye@gmail.com>
2021-11-28 23:21:35 +0900

    style(message_data.dart): クラスを分離

---
+++ lib/message_data.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+class MessageData extends StatelessWidget {
+  // ignore: use_key_in_widget_constructors
+  const MessageData({
+    Key? key,
+    required this.sender,
+    required this.text,
+    required this.isMe
+    });
+
+  final String sender;
+  final String text;
+  final bool isMe;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(10),
+      child: Column(
+        crossAxisAlignment: isMe ? CrossAxisAlignment.end : CrossAxisAlignment.start,
+        children: [
+          Text(
+            sender,
+            style: const TextStyle(
+              fontSize: 12,
+              color: Colors.black38,
+            ),
+          ),
+          Material(
+            borderRadius: isMe
+            ? const BorderRadius.only(
+                topLeft: Radius.circular(30),
+                bottomLeft: Radius.circular(30),
+                bottomRight: Radius.circular(30))
+            : const BorderRadius.only(
+                topRight: Radius.circular(30),
+                bottomLeft: Radius.circular(30),
+                bottomRight: Radius.circular(30)
+            ),
+            elevation: 5,
+            color: isMe ? Colors.lightGreenAccent : Colors.white,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+              child: Text(
+                text,
+                style: TextStyle(
+                  color: isMe ? Colors.white : Colors.black38,
+                  fontSize: 15,
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
